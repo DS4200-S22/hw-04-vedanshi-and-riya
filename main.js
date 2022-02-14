147d6469f6bc113a0c93227d4435f5ebe7019548
VedanshiShah7 <56982656+VedanshiShah7@users.noreply.github.com>
2022-02-14 08:57:35 -0500

    2 in progress

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
-let count = 0;
-
 function circle1() {
+	let count = 0;
 	count += 1;
-
 	document.getElementById("count").innerHTML = count;
+}
+
+function circle2() {
+    let curr_border = document.getElementById("circle2").getAttribute("class");
+	console.log(curr_border);
+	document.getElementById("circle2").setAttribute("class", "border2"); 
+}
+
+function circle3() {
+	let showCircle = document.getElementById("show_circle")
 }
