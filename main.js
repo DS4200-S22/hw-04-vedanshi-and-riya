582b798ebf159fd9b18cdeb25a8b54fe2b585faf
VedanshiShah7 <56982656+VedanshiShah7@users.noreply.github.com>
2022-02-15 00:02:41 -0500

    all done!

--- main.js
+++ main.js
@@ -1,18 +1,22 @@
+let count = 0;
 function circle1() {
-	let count = 0;
 	count += 1;
 	document.getElementById("count").innerHTML = count;
 }
 
-function circle2() {
-    let curr_border = document.getElementById("circle2").getAttribute("class");
-	console.log(curr_border);
-	document.getElementById("circle2").setAttribute("class", "border2"); 
+const circle2 = document.getElementById("circle2");
+function mouseOver() {
+	circle2.style.stroke = "black";
+	circle2.style.strokeWidth = 5;
+}
+
+function mouseOut() {
+	circle2.style = "null";
 }
 
 function circle3() {
 	const showCircle = document.getElementById("circle3");
-	if (showCircle.style.opacity == "0") {
+	if (showCircle.style.opacity === "0") {
 		showCircle.style.opacity = 1;
 		console.log(showCircle);
 	} else {
