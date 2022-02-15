667125c9dbc74380793ebaedb88a20541f3bce6b
VedanshiShah7 <56982656+VedanshiShah7@users.noreply.github.com>
2022-02-15 00:30:48 -0500

    Adding the comments

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
+// circle 1 function: adds 1 to the count everytime
 let count = 0;
-
 function circle1() {
 	count += 1;
 	document.getElementById("count").innerHTML = count;
 }
 
+// circle 2 function: adds black 5 border and removing the border
 const circle2 = document.getElementById("circle2");
 function mouseOver() {
 	circle2.style.stroke = "black";
@@ -15,6 +16,7 @@ function mouseOut() {
 	circle2.style = "null";
 }
 
+// circle 3 function: adds and removes the opacity of the entire shape
 function circle3() {
 	const showCircle = document.getElementById("circle3");
 	if (showCircle.style.opacity === "0") {
